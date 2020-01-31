c861de22d20eb7ed67b6d2ccbc8e7bddc84fd74c
Spencer <spencer.schien@gmail.com>
2020-01-31 16:33:49 -0600

    added choice act

--- R/database_script.R
+++ R/database_script.R
@@ -112,7 +112,7 @@ for(file in public_grad_files) {
   public_graduation <- bind_rows(public_graduation, public_graduation1)
 }
 
-# public file loop ====
+## public file loop ====
 
 public_files <- list.files(path = "imports/enrollment/public")
 
@@ -166,7 +166,7 @@ for(file in public_files) {
   public_enrollment <- bind_rows(public_enrollment, public_enrollment1)
 }
 
-# private file loop
+## private file loop ====
 
 private_files <- list.files(path = "imports/enrollment/private")
 
@@ -824,6 +824,103 @@ for(file in files) {
 forward_exam <- full_join(public_forward, choice_forward) %>%
   select(-c(MPCP, RPCP, WPCP, school_name))
 
+
+# ACT Scores ====
+
+  ## Private Schools
+
+files <- list.files(path = "./imports/wsas/private")
+
+# Set to NULL because using !exists() doesn't work in for loop
+choice_act <- choice_act1 <- NULL
+
+for (file in files) {
+  
+  filename <- paste("imports/wsas/private", file, sep = "/")
+  
+  if(length(choice_act) == 0) {
+    
+    if(str_detect(filename, "2015")) {
+      raw_choice <- read_xlsx(filename, "ACT Score Data", col_names = TRUE)
+    } else {
+      raw_choice <- read_xlsx(filename, "ACT Score Data", skip = 1, col_names = TRUE)
+      
+    }
+    
+    raw_choice <- raw_choice %>% 
+      filter(!str_detect(`School Name and Number`, "Choice Program")) %>%
+      mutate(dpi_true_id = ifelse(str_detect(`School Name and Number`, "\\("), 
+                                  str_pad(str_replace_all(str_extract(`School Name and Number`, "\\(.*\\)$"), "\\(|\\)", ""), width = 4, side = "left", pad = "0"),
+                                  str_pagd(str_sub(`School Name and Number`, start = -4, end = -1), width = 4, side = "left", pad = "0")), # this handles 2015-16 data where no () are present
+             dpi_true_id = ifelse(is.na(dpi_true_id), dpi_true_id, paste("0000", dpi_true_id, sep = "_")),
+             school_name = ifelse(!str_detect(`School Name and Number`, "-|\\("), `School Name and Number`,
+                                  str_replace(str_extract(`School Name and Number`, ".*-|.*\\("), "-|\\(", "")))
+    
+    colnames(raw_choice) <- str_to_lower(str_replace_all(colnames(raw_choice), pattern = " ", replacement = "_")) 
+    
+    scores <- raw_choice %>%
+      select(dpi_true_id, contains("average")) %>%
+      pivot_longer(cols = contains("average"), names_to = "subject", values_to = "score") %>%
+      mutate(subject = str_replace_all(subject, "average|_|score", ""))
+    
+    counts <- raw_choice %>%
+      select(dpi_true_id, contains("count")) %>%
+      pivot_longer(cols = contains("count"), names_to = "subject", values_to = "count") %>%
+      mutate(subject = str_replace_all(subject, "count|_|of", ""))
+    
+    choice_act <- left_join(scores, counts, by = c("dpi_true_id", "subject")) %>%
+      mutate(school_year = ifelse(str_detect(filename, "\\d-\\d"), str_extract(filename, "\\d{4}-\\d{2}"),
+                                  paste(as.numeric(str_extract(filename, "\\d{4}")) - 1, 
+                                        str_sub(str_extract(filename, "\\d{4}"), 3, 4), sep = "-")))
+    
+    
+  } else {
+    
+    if(str_detect(filename, "2015")) {
+      raw_choice1 <- read_xlsx(filename, "ACT Score Data", col_names = TRUE)
+    } else {
+      raw_choice1 <- read_xlsx(filename, "ACT Score Data", skip = 1, col_names = TRUE)
+      
+    }
+    
+    raw_choice1 <- raw_choice1 %>% 
+      filter(!str_detect(`School Name and Number`, "Choice Program")) %>%
+      mutate(dpi_true_id = ifelse(str_detect(`School Name and Number`, "\\("), 
+                                  str_pad(str_replace_all(str_extract(`School Name and Number`, "\\(.*\\)$"), "\\(|\\)", ""), width = 4, side = "left", pad = "0"),
+                                  str_pad(str_sub(`School Name and Number`, start = -4, end = -1), width = 4, side = "left", pad = "0")), # this handles 2015-16 data where no () are present
+             dpi_true_id = ifelse(is.na(dpi_true_id), dpi_true_id, paste("0000", dpi_true_id, sep = "_")),
+             school_name = ifelse(!str_detect(`School Name and Number`, "-|\\("), `School Name and Number`,
+                                  str_replace(str_extract(`School Name and Number`, ".*-|.*\\("), "-|\\(", "")))
+    
+    
+    colnames(raw_choice1) <- str_to_lower(str_replace_all(colnames(raw_choice1), pattern = " ", replacement = "_")) 
+    
+    scores <- raw_choice1 %>%
+      select(dpi_true_id, contains("average")) %>%
+      pivot_longer(cols = contains("average"), names_to = "subject", values_to = "score") %>%
+      mutate(subject = str_replace_all(subject, "average|_|score", ""))
+    
+    counts <- raw_choice1 %>%
+      select(dpi_true_id, contains("count")) %>%
+      pivot_longer(cols = contains("count"), names_to = "subject", values_to = "count") %>%
+      mutate(subject = str_replace_all(subject, "count|_|of", ""))
+    
+    choice_act1 <- left_join(scores, counts, by = c("dpi_true_id", "subject")) %>%
+      mutate(school_year = ifelse(str_detect(filename, "\\d-\\d"), str_extract(filename, "\\d{4}-\\d{2}"),
+                                  paste(as.numeric(str_extract(filename, "\\d{4}")) - 1, 
+                                        str_sub(str_extract(filename, "\\d{4}"), 3, 4), sep = "-")))
+  }
+  
+  
+  choice_act <- bind_rows(choice_act, choice_act1)
+  
+  
+}
+
+choice_act <- choice_act %>%
+  modify_at(c("score", "count"), as.numeric) %>%
+  modify_at("score", round, 1)
+
 # Choice Counts ====
 
 choice_names <- read_csv("imports/choice_names.csv")
@@ -880,6 +977,8 @@ dbWriteTable(school_db, "report_cards", rc_renamed, overwrite = TRUE)
 
 dbWriteTable(school_db, "forward_exam", forward_exam, overwrite = TRUE)
 
+dbWriteTable(school_db, "act", choice_act, overwrite = TRUE)
+
 dbWriteTable(school_db, "choice_counts", choice_counts, overwrite = TRUE)
 
 dbWriteTable(school_db, "other_enrollment", other_enrollment, overwrite = TRUE)
@@ -907,7 +1006,9 @@ choice_counts <- readRDS("imports/choice_counts.rds")
 
 other_enrollment <- readRDS("imports/other_enrollment.rds")
 
-save(list = c("schools", "enrollment", "report_cards", "forward_exam", "graduation", "choice_counts", "other_enrollment"),
+act <- readRDS("imports/act.rds")
+
+save(list = c("schools", "enrollment", "report_cards", "forward_exam", "graduation", "choice_counts", "other_enrollment", "act"),
      file = "C:/Users/Spencer/repor/wisconsink12/data/school_data.RData")
 
 dbDisconnect(school_db)
