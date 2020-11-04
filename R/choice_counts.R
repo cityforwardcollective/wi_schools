4afe8e3b0ba306072cd4ebfe9fff403df1cbcdc8
Spencer Schien <spencer.schien@cityforwardcollective.org>
2020-11-04 09:33:04 -0600

    added 2015-16 choice counts

--- R/choice_counts.R
+++ R/choice_counts.R
@@ -14,21 +14,40 @@ make_choice_counts <- function() {
   
   for (file in files) {
     
+    pcp_count <- str_count(str_to_lower(file), "pcp")
+    
     filename <- paste("imports/choice_counts", file, sep = "/")
     
-    cc <- readxl::read_xls(filename, skip = 4) %>%
-      filter(!is.na(`...1`)) %>%
-      select("school_name" = `School Name`,
-             "MPCP_count" = `MPCP Student HC`,
-             "RPCP_count" = `RPCP Student HC`,
-             "WPCP_count" = `WPCP \nStudent HC`,
-             "SNSP_count" = `SNSP Student HC`,
-             "ALL_STUDENTS_count" = `Total\nAll\nStudent HC`) %>%
-      mutate(school_year = substr(file, 1, 7)) %>%
-      left_join(., choice_names)
+    if (pcp_count == 1) {
+      year <- str_extract(filename, "\\d{4}-\\d{2,4}")
       
+      cc <- readxl::read_xls(filename, skip = 5) %>%
+        filter(!is.na(`...1`)) %>%
+        select("school_name" = `School Name`,
+               "MPCP_count" = `Headcount...5`,
+               "ALL_STUDENTS_count" = `Headcount...10`) %>%
+        mutate(school_year = year) %>%
+        left_join(., choice_names %>% filter(school_year == "2015-16") %>% select(-c(school_year, contains("count"))))
+      
+      
+      choice_counts <- bind_rows(choice_counts, cc)
+    } else {
+      
+      cc <- readxl::read_xls(filename, skip = 4) %>%
+        filter(!is.na(`...1`)) %>%
+        select("school_name" = `School Name`,
+               "MPCP_count" = `MPCP Student HC`,
+               "RPCP_count" = `RPCP Student HC`,
+               "WPCP_count" = `WPCP \nStudent HC`,
+               "SNSP_count" = `SNSP Student HC`,
+               "ALL_STUDENTS_count" = `Total\nAll\nStudent HC`) %>%
+        mutate(school_year = substr(file, 1, 7)) %>%
+        left_join(., choice_names)
+      
+      
+      choice_counts <- bind_rows(choice_counts, cc)
+    }
     
-    choice_counts <- bind_rows(choice_counts, cc)
   }
   
   prev_max <- choice_names %>%
