4dcd6ae1c12c2d3a6b2be10d7c62b27c74ad2903
Spencer Schien <spencer.schien@cityforwardcollective.org>
2020-11-16 10:33:27 -0600

    finished adding discipline

--- R/discipline.R
+++ R/discipline.R
@@ -1,16 +1,16 @@
 library(tidyverse)
 
-make_attendance <- function() {
+make_discipline <- function() {
   
   files <- list.files("imports/discipline/public")
   
-  attendance <- map_df(files, function(x) {
+  discipline <- map_df(files, function(x) {
     
     filename <- paste("imports/discipline/public", x, sep = "/")
     
     read_csv(filename) %>%
       filter(!SCHOOL_NAME %in% c("[Districtwide]", "[Statewide]")) %>%
-      modify_at(c("STUDENT_COUNT", "POSSIBLE_DAYS_OF_ATTENDANCE", "ACTUAL_DAYS_OF_ATTENDANCE", "ATTENDANCE_RATE"),
+      modify_at(c("TFS_ENROLLMENT_COUNT", "REMOVAL_COUNT"),
                 as.numeric) %>%
       mutate(SCHOOL_CODE = str_pad(SCHOOL_CODE, 4, side = "left", pad = "0"),
              dpi_true_id = paste(DISTRICT_CODE, SCHOOL_CODE, sep = "_")) %>%
@@ -23,9 +23,9 @@ make_attendance <- function() {
              removal_count = REMOVAL_COUNT)
   })
   
-  return(attendance)
+  return(discipline)
 }
 
 
-discipline <- make_attendance()
+discipline <- make_discipline()
 
